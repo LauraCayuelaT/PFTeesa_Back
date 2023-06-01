76a6b957f11ea5aae4e4197ee865a76839359df3
Jhonny91360 <jhonnyzamsa@hotmail.com>
2023-06-01 08:22:39 -0500

    Se agrega ruta y controler para modificar valores de un producto guardado

---
+++ src/Controllers/updateProduct.js
@@ -0,0 +1,35 @@
+const { Product } = require("../db")
+const uuidRegExp = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+const updateProduct=async(req,res)=>{
+
+    const {idProduct}=req.params
+    if(!uuidRegExp.test(idProduct)) return res.status(400).json({message: "Id invalido"}) //Validacion de uuid
+
+    const { nombre, tipo, caracteristicas, categoria, imagen, precio, stock, marca, descripcion, ref } = req.body;
+
+    try {
+        const product= await Product.findOne({ where: { id: idProduct } });
+
+        if(product){
+            product.nombre = nombre;
+            product.tipo = tipo;
+            product.caracteristicas = caracteristicas;
+            product.categoria = categoria;
+            product.imagen = imagen;
+            product.precio = precio;
+            product.stock = stock;
+            product.marca = marca;
+            product.descripcion = descripcion;
+            product.ref = ref;
+            const updatedProduct = await product.save();
+            res.status(200).json(updatedProduct)
+        }else{
+            res.status(404).json({message:"Id no encontrado"})
+        }
+    } catch (error) {
+            res.status(404).json({message:error.message})
+    }
+}
+
+module.exports=updateProduct
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const addProducts = require("../Controllers/addProducts");
 const getAllProducts = require("../Controllers/getAllProducts")
 const getDetail= require ("../Controllers/getDetail")
 const deleteProduct= require ("../Controllers/deleteProduct")
+const updateProduct= require ("../Controllers/updateProduct")
 
 // TRAE TODOS LOS PRODUCTOS DE LA BASE DE DATOS
 router.get("/products", getAllProducts)
@@ -16,5 +17,8 @@ router.post("/products", addProducts)
 
 //ELIMINA UN PRODUCTO SEGUN SU ID
 router.delete("/products/:idProduct",deleteProduct)
+
+//MODIFICA LOS VALORES DE UN PRODUCTO GUARDADO
+router.put("/detail/:idProduct",updateProduct)
 
 module.exports = router;   
