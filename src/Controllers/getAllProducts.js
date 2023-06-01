aaa16d3f5bebe63d6ddc62a653cc8d2c12b717de
Jhonny91360 <jhonnyzamsa@hotmail.com>
2023-06-01 15:27:38 -0500

    Se agrega la opcion de buscar por nombre enviado por query

--- src/Controllers/getAllProducts.js
+++ src/Controllers/getAllProducts.js
@@ -1,9 +1,26 @@
 const { Product } = require("../db");
+const { Op } = require('sequelize');
 
 const getAllProducts = async(req,res)=>{
+
     try{
-        const allProducts = await Product.findAll();
-        res.status(201).json(allProducts)
+
+        const {nombre}=req.query
+        if(nombre){
+            const searchProduct= await Product.findAll( //Busco recipes en la base de datos que contengan el name
+            {where:
+                {nombre:{
+                    [Op.iLike]: `%${nombre}%`  //Ilike no es case sensitive
+                     }
+                }               
+            })
+            res.status(201).json(searchProduct)
+        }else{
+            const allProducts = await Product.findAll();
+            res.status(201).json(allProducts)
+        }
+
+        
     }
     catch(err){res.status(404).json({message: "Catch de getAllProducts "+err.message})}
 }
