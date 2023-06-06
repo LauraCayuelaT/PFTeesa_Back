34a287c4ac82e186dbcb6c88f207c39b952bdeb0
Laura Cayuela <lauracayuela91@gmail.com>
2023-06-06 13:39:56 -0500

    Merge branch 'main' into laura

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@ const express = require ("express");
 const morgan = require("morgan");
 const routes = require("./routes/index")
 const server = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: "871749157700-dld0sob8qo0p9jranhb3misppo3g9gav.apps.googleusercontent.com",
+  clientSecret: "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",
+  callbackURL: "https://accounts.google.com/o/oauth2/auth"
+}, (accessToken, refreshToken, profile, done)=>{
+  
+}
+))
 
 server.use(express.json());
 server.use(morgan("dev"));
