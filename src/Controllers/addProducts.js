695871c55fcc33c1b45c9441f5448c253c25b864
Andres Jose Paternina Rubiano <apaterninar@gmail.com>
2023-06-10 19:38:04 -0500

    hola

--- src/Controllers/addProducts.js
+++ src/Controllers/addProducts.js
@@ -3,10 +3,10 @@ const cloudinary = require("../utils/cloudinary");
 
 const addProducts= async (req,res)=>{
 
-    const { nombre, tipo, caracteristicas, categoria, imagen, precio, stock, marca, descripcion, ref, estado } = req.body;
+    const { nombre, tipo, caracteristicas, categoria, imagenes, precio, stock, marca, descripcion, ref, estado } = req.body;
         
 
-    if(!nombre || !imagen || !precio || !marca || !ref) return res.status(404).json({message: "Faltan datos"})
+    if(!nombre || !imagenes || !precio || !marca || !ref) return res.status(404).json({message: "Faltan datos"})
 
     const proExist = await Product.findOne({where:{nombre, ref}});
     console.log(proExist);
@@ -14,16 +14,16 @@ const addProducts= async (req,res)=>{
     
 
     try{
-        const cloudinaryResponse = await cloudinary.uploader.upload(imagen, {
-            folder: 'products' 
-          });
-          const imageUrl = cloudinaryResponse.secure_url;
-
-    const newProduct = await Product.create({nombre, tipo, caracteristicas, categoria, imagen:imageUrl, precio, stock, marca, descripcion, ref, estado})
-    
-
+        const uploadedImages = [];
+        for (const imagen of imagenes) {
+            const cloudinaryResponse = await cloudinary.uploader.upload(imagen, {
+        folder: 'products'
+      });
+      const imageUrl = cloudinaryResponse.secure_url;
+      uploadedImages.push(imageUrl);
+    }
+    const newProduct = await Product.create({nombre, tipo, caracteristicas, categoria, imagenes:uploadedImages, precio, stock, marca, descripcion, ref, estado})
     res.status(200).json(newProduct)
-
     }catch(err){res.status(404).json({message: "Llegue al catch del post de productos "+ err.message})}
 
 
