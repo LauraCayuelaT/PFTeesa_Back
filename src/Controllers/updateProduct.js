6b6e11be4b1c41f802095336b11b78d229a9d5e7
Jhonny91360 <jhonnyzamsa@hotmail.com>
2023-06-21 19:45:02 -0500

    No se que he tocado

--- src/Controllers/updateProduct.js
+++ src/Controllers/updateProduct.js
@@ -6,23 +6,26 @@ const updateProduct=async(req,res)=>{
     const {idProduct}=req.params
     if(!uuidRegExp.test(idProduct)) return res.status(400).json({message: "Id invalido"}) //Validacion de uuid
 
-    const { nombre, tipo, caracteristicas, categoria, imagenes, precio, stock, marca, descripcion, ref, estado } = req.body;
+    const {  imagenes, precio, stock} = req.body;
 
     try {
         const product= await Product.findOne({ where: { id: idProduct } });
 
         if(product){
-            product.nombre = nombre;
-            product.tipo = tipo;
-            product.caracteristicas = caracteristicas;
-            product.categoria = categoria;
-            product.imagenes = imagenes;
-            product.precio = precio;
-            product.stock = stock;
-            product.marca = marca;
-            product.descripcion = descripcion;
-            product.ref = ref;
-            product.estado = estado;
+            product.imagenes = imagenes? imagenes : product.imagenes;
+            product.precio = precio? precio:product.precio;
+            product.stock = stock? stock: product.stock;
+            // product.nombre = nombre;
+            // product.tipo = tipo;
+            // product.caracteristicas = caracteristicas;
+            // product.categoria = categoria;
+            // product.imagenes = imagenes;
+            // product.precio = precio;
+            // product.stock = stock;
+            // product.marca = marca;
+            // product.descripcion = descripcion;
+            // product.ref = ref;
+            // product.estado = estado;
             const updatedProduct = await product.save();
             res.status(200).json(updatedProduct)
         }else{
